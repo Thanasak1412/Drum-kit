9777152eabde3a0c93e6e3c46530fbb19620261a
Thanasak1412 <58911941+Thanasak1412@users.noreply.github.com>
2021-04-09 02:09:46 +0700

    update effect press

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 (() => {
+  const drumElements = document.querySelectorAll(".drum");
+
+  function removePressed(element) {
+    setTimeout(() => {
+      element.classList.remove("pressed");
+    }, 100);
+  }
+
   function playDrum(event) {
     const tom1Audio = new Audio("./sounds/tom-1.mp3");
     const tom2Audio = new Audio("./sounds/tom-2.mp3");
@@ -9,30 +17,44 @@
     const kickAudio = new Audio("./sounds/kick-bass.mp3");
     let drum = event.target;
     let keyCode = event.key;
+    const drumEvent = document.querySelectorAll(".drum");
 
     if (drum.classList.contains("w") || keyCode === "w") {
       tom1Audio.play();
+      drumEvent[0].classList.add("pressed");
+      removePressed(drumEvent[0]);
     } else if (drum.classList.contains("a") || keyCode === "a") {
       tom2Audio.play();
+      drumEvent[1].classList.add("pressed");
+      removePressed(drumEvent[1]);
     } else if (drum.classList.contains("s") || keyCode === "s") {
       tom3Audio.play();
+      drumEvent[2].classList.add("pressed");
+      removePressed(drumEvent[2]);
     } else if (drum.classList.contains("d") || keyCode === "d") {
       tom4Audio.play();
+      drumEvent[3].classList.add("pressed");
+      removePressed(drumEvent[3]);
     } else if (drum.classList.contains("j") || keyCode === "j") {
       snareAudio.play();
+      drumEvent[4].classList.add("pressed");
+      removePressed(drumEvent[4]);
     } else if (drum.classList.contains("k") || keyCode === "k") {
       crashAudio.play();
+      drumEvent[5].classList.add("pressed");
+      removePressed(drumEvent[5]);
     } else if (drum.classList.contains("l") || keyCode === "l") {
       kickAudio.play();
+      drumEvent[6].classList.add("pressed");
+      removePressed(drumEvent[6]);
     }
   }
 
   function run() {
-    const drumElements = document.querySelectorAll(".drum");
     drumElements.forEach((drumElement) => {
       drumElement.addEventListener("click", playDrum);
-      document.addEventListener("keydown", playDrum);
     });
+    document.addEventListener("keydown", playDrum);
   }
 
   run();
